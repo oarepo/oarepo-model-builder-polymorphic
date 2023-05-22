a9d2e46a852c0f5a0f6577643d7a138d9d3ccc30
Mirek Simek <miroslav.simek@gmail.com>
2023-05-22 12:16:03 +0200

    tests working

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+if [ -d .venv-builder ] ; then
+  rm -rf .venv-builder
+fi
+
+python3 -m venv .venv-builder
+.venv-builder/bin/pip install -U setuptools pip wheel
+
 .venv-builder/bin/pip install -e .
 
 if [ -d built-record ] ; then 
@@ -10,13 +17,13 @@ fi
 
 .venv-builder/bin/oarepo-compile-model -vvv tests/model.yaml --output-directory built-record
 
- if [ -d .venv ] ; then
-     rm -rf .venv
+ if [ -d .venv-tests ] ; then
+     rm -rf .venv-tests
  fi
 
- python3 -m venv .venv
+ python3 -m venv .venv-tests
 
- source .venv/bin/activate
+ source .venv-tests/bin/activate
  pip install -r tests/requirements.txt
  pip install -e ./built-record
 
